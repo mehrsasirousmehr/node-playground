b47cf53a554b5ff24f3ba6cbe2f0dcc6adc9890d
mehrsa_s <m.sirousmehr@tapsell.ir>
2024-11-12 18:53:22 +0330

    try for find Arrow prototype

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ console.log('-------------------- index.js --------------------')
 Array.prototype.customMap = customMap;
 Array.prototype.customFind = function (callback) {
     // TODO: ...
+    for (let i = 0; i < this.length; i++){
+        if (callback(this[i])) {
+            return callback(this[i]);
+        }
+    }
     return undefined;
 }
 
-const numbers = [5, 12, 8, 145, 4]
+// const numbers = [5, 12, 8, 145, 4]
+const numbers = [12,4,55,66]
 
-console.log(numbers.customMap(n => n * 2))
+// console.log(numbers.customMap(n => n * 2))
 console.log(numbers.customFind(n => n * 2))
 
-const result = numbers.find(number => number > 10)
-console.log(result)
+// const result = numbers.find(number => number > 10)
+// console.log(result)
 
 
